a832804a1afd10596acb179c81af75fe66cdc28b
Bruno Augusto <bruno.augusto@siemens.com>
2024-08-15 13:06:20 +0100

    Remove ExtractMovieDeatils method from Helper.cs

--- src/FilmowToLetterboxdCSV/Helper.cs
+++ src/FilmowToLetterboxdCSV/Helper.cs
@@ -87,55 +87,6 @@ namespace FilmowToLetterboxdCSV
             return movie;
         }
 
-        internal async static Task<IEnumerable<Movie>> ExtractMovieDeatils(this IBrowsingContext context, IEnumerable<(string link, string rating)> links)
-        {
-            List<Movie> result = [];
-
-            foreach (var (link, rating) in links)
-            {
-                Console.WriteLine($"\n{link}");
-
-                using var document = await context.OpenAsync($"https://filmow.com{link}");
-
-                var title = document.QuerySelectorAll("div.movie-other-titles > ul > li")?
-                    .Select(li =>
-                    {
-                        if (li.QuerySelector("em")?.TextContent == "Estados Unidos da América")
-                        {
-                            var text = li.QuerySelector("strong")?.TextContent ?? string.Empty;
-                            return RegexIsASCII().IsMatch(text) ? text : null;
-                        }
-
-                        return null;
-                    })
-                    .FirstOrDefault(title => !string.IsNullOrEmpty(title));
-
-                if (string.IsNullOrEmpty(title))
-                {
-                    var text = document.QuerySelector("div.movie-title > div > h2.movie-original-title")?.TextContent ?? string.Empty;
-                    title = RegexIsASCII().IsMatch(text) ? text : null;
-                }
-
-                if (string.IsNullOrEmpty(title))
-                    title = document.QuerySelector("div.movie-title > h1")?.TextContent;
-
-                if (string.IsNullOrEmpty(title)) continue;
-
-                var year = document.QuerySelector("small.release")!.TextContent;
-
-                var director = document.QuerySelector("div.directors > span > a > span[itemprop='name']")?.TextContent ?? string.Empty;
-
-                result.Add(new Movie(title, director, year, rating));
-
-                if (title.Contains(","))
-                    title = $"\"{title}\"";
-
-                Console.WriteLine($"{title} ({year}) - {director}");
-            }
-
-            return result;
-        }
-
         [GeneratedRegex(@"([\d\.]+) estrelas")]
         private static partial Regex RegexRating();
         [GeneratedRegex(@"[\x00-\x7F]+")]
